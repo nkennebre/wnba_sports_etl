0ef25d49bd744f0d328b130006fdd856cbdec8bb
KennebrewS <maxine@gekotinc.com>
2025-06-12 19:05:21 -0400

    Added a clean 10 year dataset. Added the function to transform the 10 year dataset. Added the License.

--- sports_etl/etl/transform.py
+++ sports_etl/etl/transform.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 logger = get_logger(__name__)
 
-def transform_team_stats(df):
+def transform_team_stats_kaggle(df):
     """
     Transform raw WNBA team stats into engineered, clean format.
     """
@@ -22,6 +22,31 @@ def transform_team_stats(df):
     df["possessions"] = df["fga"] + 0.44 * df["fta"] - df["oreb"] + df["tov"]
     df["off_rating"] = df["ppg"] / df["possessions"].replace(0, pd.NA) * 100
     
+    # Drop rows with any resulting NAs
+    df.dropna(inplace=True)
+    
+    # Drop exact duplicate rows (across all columns)
+    df.drop_duplicates(inplace=True)
+
+    logger.info(f"Transformed team stats — final shape after deduplication: {df.shape}")
+    logger.info("Transformed team stats with new features.")
+    return df
+
+def transform_team_stats_my_dataset(df):
+    """
+    Transform raw WNBA team stats into engineered, clean format.
+    """
+    df = df.copy()
+    
+    # Add features
+    df["win_margin"] = df["Tm_Pts"] - df["Opp_Pts"]
+    df["Tm_poss"] = df["Tm_FGA"] + 0.44 * df["Tm_FTA"] - df["Tm_ORB"] + df["Tm_TOV"]
+    df["Opp_poss"] = df["Opp_FGA"] + 0.44 * df["Opp_FTA"] - df["Opp_ORB"] + df["Opp_TOV"]
+    df["Tm_off_rating"] = (df["Tm_Pts"] / df["Tm_poss"].replace(0, pd.NA)) * 100
+    df["Tm_def_rating"] = (df["Opp_Pts"] / df["Opp_poss"].replace(0, pd.NA)) * 100
+    df["Tm_tov_rate"] = df["Tm_TOV"]/df["Tm_poss"]
+    df["Opp_tov_rate"] = df["Opp_poss"]/df["Opp_poss"]
+     
     # Drop rows with any resulting NAs
     df.dropna(inplace=True)
     
